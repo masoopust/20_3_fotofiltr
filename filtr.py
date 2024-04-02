40c74fb21c6a5be422b5ae4794f8a7314b4c11d8
masoopust <patrikmasopust@seznam.cz>
2024-04-02 16:38:08 +0200

    Translate and design

--- filtr.py
+++ filtr.py
@@ -1,66 +1,68 @@
 from PIL import Image
 
-def fialovyf():
-    obrazek = Image.open("plaz.jpg")
-    sirka, vyska = obrazek.size
+def purple():
+    picture = Image.open("beach.jpg")
+    width, high = picture.size
     x = 0
-    while x < sirka:
+    while x < width:
         y = 0
-        while y < vyska:
-            r, g, b = obrazek.getpixel((x,y))
-            prumer = int((r+g+b)/3)
-            obrazek.putpixel((x,y), (r , b, r))
+        while y < high:
+            r, g, b = picture.getpixel((x,y))
+            diameter = int((r+g+b)/3)
+            picture.putpixel((x,y), (r , b, r))
             y += 1
         x += 1
-    obrazek.show()
+    picture.show()
 
-def teple_barevny_filtr():
-    obrazek = Image.open("plaz.jpg")
-    sirka, vyska = obrazek.size
+
+def warm_colors():
+    picture = Image.open("beach.jpg")
+    width, high = picture.size
     x = 0
-    while x < sirka:
+    while x < width:
         y = 0
-        while y < vyska:
-            r, g, b = obrazek.getpixel((x,y))
-            # Úprava barev pro teplý tón
-            r_new = min(255, r + 50)  # Zvýšíme červenou složku o 50
-            g_new = min(255, g + 30)  # Zvýšíme zelenou složku o 30
-            b_new = max(0, b - 30)    # Snížíme modrou složku o 30
-            obrazek.putpixel((x,y), (r_new, g_new, b_new))
+        while y < high:
+            r, g, b = picture.getpixel((x,y))
+             # Color adjustment for warmer tone
+            r_new = min(255, r + 50)  # Increase of the red component by 50
+            g_new = min(255, g + 30)  # Increase of the green component by 30
+            b_new = max(0, b - 30)    # Reduce of the blue component by 50
+            picture.putpixel((x,y), (r_new, g_new, b_new))
             y += 1
         x += 1
-    obrazek.show()   
+    picture.show()
 
 
-def cernobily():
-    obrazek = Image.open("plaz.jpg")
-    sirka, vyska = obrazek.size
+def blckwhite():
+    picture = Image.open("beach.jpg")
+    width, high = picture.size
     x = 0
-    while x < sirka:
+    while x < width:
         y = 0
-        while y < vyska:
-            r, g, b = obrazek.getpixel((x,y))
-            # Vypočítáme průměr hodnot R, G, B a použijeme ho pro všechny složky
-            prumer = int((r + g + b) / 3)
-            # Nastavíme novou hodnotu barvy na odstíny šedé
-            obrazek.putpixel((x,y), (prumer, prumer, prumer))
+        while y < high:
+            r, g, b = picture.getpixel((x,y))
+            # Calculate the average of R, G, B values ​​and use it for all components
+            diameter = int((r + g + b) / 3)
+            # Set the new value for gray shadows 
+            picture.putpixel((x,y), (diameter, diameter, diameter))
             y += 1
         x += 1
-    obrazek.show()
+    picture.show()
 
-oddelovac = "=" * 62
-sluzby = ("fialovy filtr - 1", "teply filtr - 2", "cerrnobily filtr - 3")
-print("Vitejte v nasem filtrovem studiu!".upper().center(62),"Jaký filtr by jsi chtěl na fotku dát?".lower().center(62),
+oddelovac = "=" * 70
+types = ("Purple filter - 1", "Warm filter - 2", "Black and white filter - 3")
+print("\n")
+print("Welcome in mine filter studio!".upper().center(70),"Which filter would you like to apply on photo?".lower().center(62),
           oddelovac,sep="\n")
-print(f"| {' | '.join(sluzby)} |".center(62), oddelovac, sep="\n")
+print(f"| {' | '.join(types)} |".center(62), oddelovac, sep="\n")
 
-volba = int(input("1,2,3 ?"))
+volba = int(input("1,2,3 ?\n: "))
 
 if (volba == 1):
-    fialovyf()
+    purple()
 
 if (volba == 2):
-    teple_barevny_filtr()
+    warm_colors()
 
 if (volba == 3):
-    cernobily()    
+    blckwhite()    
